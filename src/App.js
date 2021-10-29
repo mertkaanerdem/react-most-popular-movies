9618cb63a0d906e4392aa6a16637129577eff244
mertkaanerdem <mmertkaanerdem@gmail.com>
2021-10-29 22:18:09 +0300

    routing and components are done for now

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Nav from "./components/Navigation";
+import Movies from "./components/Movies";
+import Movie from "./components/Movie";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Nav />
+      <Switch>
+        <Route path="/" component={Movies} exact />
+        <Route path="/:id" component={Movie} />
+      </Switch>
+    </Router>
   );
 }
 
