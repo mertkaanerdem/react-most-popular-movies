c4f7c05ee6ee3d1ca5d4e5a8df9bf973b8b211bf
mertkaanerdem <mmertkaanerdem@gmail.com>
2021-10-30 02:43:52 +0300

    movie component

--- src/components/Movie/index.js
+++ src/components/Movie/index.js
@@ -1,7 +1,59 @@
-import React from "react";
+import { useParams, Link } from "react-router-dom";
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
+
+import { useMovies } from "../../context/MoviesContext";
 
 function Movie() {
-  return <div>Movie</div>;
+  const { id } = useParams();
+  const { movies } = useMovies();
+
+  const filteredMovie = movies.filter((movie) => movie.id === id);
+  return (
+    <Container className="App mt-2">
+      <Row xs={1} className="g-4">
+        {filteredMovie.map((movie) => (
+          <Col key={movie.id} className="text-center">
+            <Card
+              style={{ width: "24vw", margin: "0 auto" }}
+              className="shadow relative"
+            >
+              <Card.Img
+                className="px-4 pt-2"
+                variant="top"
+                src={movie.image}
+                alt={movie.title}
+              />
+              <Card.Body>
+                <Col className="title">
+                  <Card.Title>{movie.title}</Card.Title>
+                  {movie.imDbRating && (
+                    <span className="rating bg-warning rounded-circle p-2 me-1">
+                      {movie.imDbRating}
+                    </span>
+                  )}
+                  <Card.Subtitle>
+                    <span className="year bg-warning  py-2 rounded-end">
+                      {movie.year}
+                    </span>
+                  </Card.Subtitle>
+                </Col>
+
+                <Card.Text className="crew text-center p-2">
+                  {movie.crew.split(",").join(" ~ ")}
+                </Card.Text>
+
+                <Link to="/">
+                  <div className="d-grid ">
+                    <Button variant="primary">Go To Home Page</Button>
+                  </div>
+                </Link>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
 }
 
 export default Movie;
