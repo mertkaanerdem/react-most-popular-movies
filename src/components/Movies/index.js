f3a6d42f31c567d0662447368a666c32e3938620
mertkaanerdem <mmertkaanerdem@gmail.com>
2021-10-30 17:46:44 +0300

    new card style

--- src/components/Movies/index.js
+++ src/components/Movies/index.js
@@ -8,54 +8,55 @@ function Movies() {
 
   return (
     <div class="divContainer">
-    <Container className="App mt-2">
-      {isLoading && (
-        <Spinner animation="grow" variant="warning" className="spinner" />
+      <Container className="App mt-2">
+        {isLoading && (
+          <Spinner animation="grow" variant="warning" className="spinner" />
         )}
-      <Row xs={1} md={3} xl={4} className="g-4">
-        {movies.map((movie) => (
-          <Col key={movie.id} className="text-center">
-            <Card
-              style={{ margin: "0 auto" }}
-              className="shadow relative carding"
-            >
-              <Card.Img
-                className="px-4 pt-2"
-                variant="top"
-                src={movie.image}
-                alt={movie.title}
+        <Row xs={1} md={2} xl={3} className="g-4">
+          {movies.map((movie) => (
+            <Col key={movie.id} className="text-center">
+              <Card style={{ margin: "0 auto" }} className="shadow carding">
+                <Card.Img
+                  className="px-4 absolute image"
+                  variant="top"
+                  src={movie.image}
+                  alt={movie.title}
+                  // width="100"
+                  // height="300"
                 />
-              <Card.Body>
-                <Col className="title">
-                  <Card.Title>{movie.title}</Card.Title>
-                  {movie.imDbRating && (
-                    <span className="rating bg-warning rounded-circle p-2 me-1">
-                      {movie.imDbRating}
-                    </span>
-                  )}
-                  <Card.Subtitle>
-                    <span className="year bg-warning  py-2 rounded-end">
-                      {movie.year}
-                    </span>
-                  </Card.Subtitle>
-                </Col>
+                <Card.Body>
+                  <Col className="relative">
+                    <Card.Title className="bg-warning p-1 title rounded">
+                      {movie.title}
+                    </Card.Title>
+                    {movie.imDbRating && (
+                      <span className="rating bg-warning rounded-circle p-2 me-1">
+                        {movie.imDbRating}
+                      </span>
+                    )}
+                    <Card.Subtitle>
+                      <span className="year bg-warning  py-2 rounded-end">
+                        {movie.year}
+                      </span>
+                    </Card.Subtitle>
+                  </Col>
 
-                <Card.Text className="crew text-center p-2">
-                  {movie.crew.split(",").join(" ~ ")}
-                </Card.Text>
+                  <Card.Text className="crew text-center p-2">
+                    {movie.crew.split(",").join(" ~ ")}
+                  </Card.Text>
 
-                <Link to={`/${movie.id}`}>
+                  <Link to={`/${movie.id}`}>
                     <Button className="button" variant="primary">
                       Go To The Film's Page
                     </Button>
-                </Link>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
-      </Row>
-    </Container>
-                  </div>
+                  </Link>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </div>
   );
 }
 
